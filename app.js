088c0b473e23dfaa9e67f6295ae4fabacda254f0
Jaime Lizama R <jlizama@poptimize.cl>
2021-07-24 00:29:05 -0400

    Agregando métodos
    Revisión de puntos faltantes

--- app.js
+++ app.js
@@ -1,51 +1,15 @@
-//////////////////////////////////////////
-// Ejemplo para visualizar los objetos
-
-// function Clinic(name, patient) {
-//   this.name = name;
-//   this.patient = patient;
-// }
-
-// function Patient(name, age, rut, diagnosis) {
-//   this.name = name;
-//   this.age = age;
-//   this.rut = rut;
-//   this.diagnosis = diagnosis;
-// }
-
-// let p1 = new Patient("Juan", 20, "10123456-1", "Sano");
-// let c1 = new Clinic("CLC", p1);
-
-// console.log(c1);
-// console.log(p1);
-
-//////////////////////////////////////////
 // Desarrollo de Desafío
 
-function Clinic(name, patient) {
-  let _name = name;
-  let _patient = patient;
-
-  Object.defineProperty(this, "_name", {
-    get: function () {
-      return _name;
-    },
-    set: function (newName) {
-      _name = newName;
-    },
-  });
-  Object.defineProperty(this, "_patient", {
-    get: function () {
-      return _patient;
-    },
-  });
+function Clinic(clinicName, patient) {
+  this._clinicName = clinicName;
+  this._patient = patient;
 }
 
 function Patient(name, age, rut, diagnosis) {
-  let _name = name;
-  let _age = age;
-  let _rut = rut;
-  let _diagnosis = diagnosis;
+  var _name = name;
+  var _age = age;
+  var _rut = rut;
+  var _diagnosis = diagnosis;
 
   Object.defineProperty(this, "_name", {
     get: function () {
@@ -113,19 +77,35 @@ Patient.prototype.setPatientDiagnosis = function (newDiagnosis) {
 };
 
 Clinic.prototype.getClinicName = function () {
-  return this._name;
+  return this._clinicName;
 };
 
-Clinic.prototype.setClinicName = function (newName) {
-  return (this._name = newName);
+Clinic.prototype.setClinicName = function (newClinicName) {
+  return (this._clinicName = newClinicName);
 };
 
 Clinic.prototype.getClinicPatient = function () {
   return this._patient;
 };
 
-let patient1 = new Patient("Juan", 20, "10123456-1", "Sano");
-let clinic1 = new Clinic("CLC", patient1);
+Clinic.prototype.searchForPatient = function (PatientNametoSearch) {
+  var index = -1;
+  for (var i = 0; i < this._patient.length; i++) {
+    if (this._patient[i]._name == PatientNametoSearch) {
+      index = i;
+    }
+  }
+
+  if (index >= 0) {
+    console.log(this._patient[index]._name);
+  } else {
+    console.log("Paciente no encontrado");
+  }
+};
+
+const patient1 = new Patient("Juan", 20, "10123456-1", "Sano");
+const patient2 = new Patient("Coté", 22, "12123456-1", "Enfermo");
+const clinic1 = new Clinic("CLC", [patient1, patient2]);
 
 console.log(clinic1);
 console.log(patient1);
@@ -137,3 +117,33 @@ console.log(patient1.getPatientRut());
 console.log(patient1.getPatientDiagnosis());
 patient1.setPatientName("Javiera");
 console.log(patient1.getPatientName());
+patient1.setPatientAge(25);
+console.log(patient1.getPatientAge());
+patient1.setPatientRut("11456789-0");
+console.log(patient1.getPatientRut());
+patient1.setPatientDiagnosis("Más o menos");
+console.log(patient1.getPatientDiagnosis());
+clinic1.searchForPatient("Coté");
+clinic1.searchForPatient("Juan");
+clinic1.searchForPatient("Javiera");
+
+//////////////////////////////////////////
+// Ejemplo para visualizar los objetos
+
+// function Clinic(name, patient) {
+//   this.name = name;
+//   this.patient = patient;
+// }
+
+// function Patient(name, age, rut, diagnosis) {
+//   this.name = name;
+//   this.age = age;
+//   this.rut = rut;
+//   this.diagnosis = diagnosis;
+// }
+
+// let p1 = new Patient("Juan", 20, "10123456-1", "Sano");
+// let c1 = new Clinic("CLC", p1);
+
+// console.log(c1);
+// console.log(p1);
